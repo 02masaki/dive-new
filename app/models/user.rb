3f1908ddc55a7fd84e78fe78919d5a102256094f
02masaki <69668107+02masaki@users.noreply.github.com>
2020-09-29 22:31:18 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  EMAIL_VAL = /\A\S+@\S+\.\S+\z/
+  PASS_VAL = /\A[a-zA-Z0-9]+\z/
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, format: {with: EMAIL_VAL}
+    validates :password, format: {with: PASS_VAL}
+  end
 end
